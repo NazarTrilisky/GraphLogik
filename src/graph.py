39d8b6588d23225d70ad7874bc2ab41370e25f59
Naz <my@email.com>
2021-10-30 14:53:31 -0400

    Basic graph added

---
+++ src/graph.py
@@ -0,0 +1,36 @@
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+class KnowledgeGraph:
+    def __init__(self):
+        self.graph = nx.Graph()
+
+    def addNode(self, text):
+        self.graph.add_node(text.strip().lower())
+
+    def connect(self, n1, n2, weight=1):
+        self.graph.add_weighted_edges_from([(n1, n2, weight)])
+
+    def show(self):
+        s_layout = nx.spring_layout(self.graph)
+        nx.draw(self.graph, s_layout, with_labels=True)
+        node_labels = nx.get_node_attributes(self.graph, 'weight')
+        nx.draw_networkx_labels(self.graph, s_layout, labels = node_labels)
+        edge_labels = nx.get_edge_attributes(self.graph, 'weight')
+        nx.draw_networkx_edge_labels(self.graph, s_layout, edge_labels)
+        plt.show()
+
+
+if __name__ == '__main__':
+    kg = KnowledgeGraph()
+    kg.addNode("brush")
+    kg.addNode("teeth")
+    kg.addNode("floss")
+    kg.addNode("cavities")
+    kg.connect("brush", "teeth")
+    kg.connect("floss", "teeth", 1.5)
+    kg.connect("brush", "cavities", -1)
+    kg.show()
+
--- src/text_to_graph.py
+++ src/text_to_graph.py
@@ -1,17 +1,14 @@
 
 import spacy
-import networkx as nx
+
+from graph import KnowledgeGraph
 
 
 nlp = spacy.load('en_core_web_sm')
-doc = nlp("The quick brown fox jumped over the lazy, fat dog.")
 
-tokens = [t for t in doc]
+
+tokens = nlp("The quick brown fox jumped over the lazy, fat dog.")
 for token in tokens:
-    print("{} - {}, ".format(token.text, token.pos))
+    print("%s, %s, %s" % (token.text, token.pos_, token))
 
-
-G = nx.Graph()
-G.add_node(1)
-print(G)
 
