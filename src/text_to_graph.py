cd4a160454a946f9c33cb36411f2298e2891b4c3
Naz <my@email.com>
2021-10-31 00:40:40 -0400

    Clean debug lines

--- src/text_to_graph.py
+++ src/text_to_graph.py
@@ -9,19 +9,15 @@ tokens = nlp("The quick brown fox jumped over the lazy, fat dog.")
 
 def link_related_nodes(kg, token_idx):
     """ Link adjectives and verbs and subj/obj """
-    print(token_idx)
     if tokens[token_idx].pos_ in [u'NOUN', u'PROPN']:
-        print("is nounish: %s" % tokens[token_idx])
         # go backwards
         for idx in range(token_idx-1, -1, -1):
             if tokens[idx].pos_ in [u'ADJ', u'advcl', u'VERB']:
-                print("link")
                 kg.connect(tokens[token_idx].text, tokens[idx].text, weight=1)
 
         # go forwards
         for idx in range(token_idx+1, 1, len(tokens)):
             if tokens[idx].pos_ in [u'VERB']:
-                print("link2")
                 kg.connect(tokens[token_idx].text, tokens[idx].text, weight=1)
 
 
