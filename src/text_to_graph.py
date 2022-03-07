0465cd8f8debb523a21a66c7dc86f2c267709c35
Naz <noreply@github.com>
2022-03-06 21:09:40 -0500

    Pivot on all verbs, not just ROOT

--- src/text_to_graph.py
+++ src/text_to_graph.py
@@ -13,11 +13,17 @@ def translate_text_into_graph(kg, text):
     Adds nodes and relationships from the sentences into knowledge graph (kg)
     """
     new_text = replace_pronouns(text)
+    new_text = new_text.replace(';', '.')
     sentences = new_text.split(".")
 
     for sentence in sentences:
         tokens_w_stopwords = nlp(sentence)
         tokens = [token for token in tokens_w_stopwords if not token.text in stopwords]
+
+        #print("\n\n")
+        #for t in tokens:
+        #    print(t, t.pos_, t.dep_)
+
         adj_list = []
         subj = None
         obj = None
@@ -35,26 +41,45 @@ def translate_text_into_graph(kg, text):
                     kg.connect(token.lemma_, descr_token.lemma_)
                 descr_tokens.clear()
 
-        # get root verb and connect main action, subj, obj
+        def get_subj_obj(idx):
+            """
+            Return tuple with two lists ([subjects], [objects])
+            for the verb at the given index
+            """
+            subjects = []
+            objects = []
+            cur = idx - 1
+            # move backwards to find subjects
+            while cur >= 0 and tokens[cur].dep_ != 'VERB':
+                if tokens[cur].dep_.endswith('subj'):
+                    subjects.append(tokens[cur])
+                cur -= 1
+
+            # move forward to find objects
+            cur = idx + 1
+            while cur < len(tokens) and tokens[cur].dep_ != 'VERB':
+                if tokens[cur].dep_.endswith('obj'):
+                    objects.append(tokens[cur])
+                cur += 1
+
+            return (subjects, objects)
+
+        # get verb and connect main action, subj, obj
         subj_tok = []
         obj_tok = []
         verb_tok = None
-        for token in tokens:
-            if token.dep_ == 'ROOT':
-                verb_tok = token
-            elif token.dep_.endswith('subj'):
-                subj_tok.append(token)
-            elif token.dep_.endswith('obj'):
-                obj_tok.append(token)
-
-        if verb_tok:
-            kg.addNode(verb_tok.lemma_)
-            for st in subj_tok:
-                kg.addNode(st.lemma_)
-                kg.connect(st.lemma_, verb_tok.lemma_)
-            for ot in obj_tok:
-                kg.addNode(ot.lemma_)
-                kg.connect(ot.lemma_, verb_tok.lemma_)
+        for idx in range(len(tokens)):
+            token = tokens[idx]
+            if token.pos_ == 'VERB':
+                subjects, objects = get_subj_obj(idx)
+                print("verb: {}, subj: {}, obj: {}".format(token, subjects, objects))
+                kg.addNode(token.lemma_)
+                for subj in subjects:
+                    kg.addNode(subj.lemma_)
+                    kg.connect(subj.lemma_, token.lemma_)
+                for obj in objects:
+                    kg.addNode(obj.lemma_)
+                    kg.connect(obj.lemma_, token.lemma_)
 
 
 if __name__ == '__main__':
