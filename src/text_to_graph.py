1437230fcf1fcfc788e068fae974cb8e632eeba3
Naz <noreply@github.com>
2022-04-17 22:09:45 -0400

    Pronoun replacement

--- src/text_to_graph.py
+++ src/text_to_graph.py
@@ -2,33 +2,11 @@
 import spacy
 from src.graph import KnowledgeGraph
 
+from src.pronouns import update_last_noun, get_text_for_pronoun
+
 
 nlp = spacy.load('en_core_web_sm')
 stopwords = ['a', 'the', 'for', 'at', 'by']
-
-#Pronoun replace
-last_masculaine_entity  = None
-last_feminine_entity    = None
-last_plural_entity      = None
-feminine_pronouns =     ['she', 'her', 'hers', 'herself']
-masculaine_pronouns =   ['he', 'him' 'his', 'himself']
-neutral_pronouns =      ['it', 'its', 'itself']
-plural_pronouns =       ['they', 'them', 'their', 'theirs', 'themselves']
-
-
-def replace_pronoun(token):
-    """
-    Replace pronoun token with the string of what it refers to
-    """
-    global last_masculaine_entity
-    global last_feminine_entity
-    global last_plural_entity
-    global feminine_pronouns
-    global masculaine_pronouns
-    global neutral_pronouns
-    global plural_pronouns
-
-    return "PRONOUN"
 
 
 def add_adjectives_to_graph(kg, tokens):
@@ -59,18 +37,24 @@ def get_subj_obj(tokens, idx):
     cur = idx - 1
     # move backwards to find subjects
     while cur >= 0 and tokens[cur].dep_ != 'VERB':
-        if tokens[cur].dep_.endswith('subj'):
+        if tokens[cur].dep_.endswith('subj') and tokens[cur].text.strip():
             subjects.append(tokens[cur])
         cur -= 1
 
     # move forward to find objects
     cur = idx + 1
     while cur < len(tokens) and tokens[cur].dep_ != 'VERB':
-        if tokens[cur].dep_.endswith('obj'):
+        if tokens[cur].dep_.endswith('obj') and tokens[cur].text.strip():
             objects.append(tokens[cur])
         cur += 1
 
     return (subjects, objects)
+
+
+def get_token_text(token):
+    if token.pos_ == 'PRON':
+        return get_text_for_pronoun(token)
+    return token.text
 
 
 def translate_text_into_graph(kg, text):
@@ -87,43 +71,43 @@ def translate_text_into_graph(kg, text):
 
         print("\n\n")
         for t in tokens:
-            print(t, t.pos_, t.dep_, t.text)
+            print("%s, %s, %s" % (t, t.pos_, t.dep_))
 
-        adj_list = []
-        subj = None
-        obj = None
-        verb = None
-
+        # Add adjectives to nouns
         add_adjectives_to_graph(kg, tokens)
 
-        # get verb and connect main action, subj, obj
-        subj_tok = []
-        obj_tok = []
-        verb_tok = None
-
+        # Add subj-verb-obj triples
         for idx in range(len(tokens)):
             token = tokens[idx]
-            if token.pos_ == 'VERB':
+            if tokens[idx].pos_ == 'NOUN':
+                #print("noun: %s, lemma: %s" % (tokens[idx], tokens[idx].lemma_))
+                update_last_noun(tokens, idx)
+            elif token.pos_ == 'VERB':
                 verb_token = token
                 subjects, objects = get_subj_obj(tokens, idx)
                 print("verb: {}, subj: {}, obj: {}".format(verb_token, subjects, objects))
-                for subj in subjects:
+
+                if subjects and objects:
+                    for subj in subjects:
+                        for obj in objects:
+                            kg.addNode(get_token_text(subj))
+                            kg.addNode(get_token_text(obj))
+                            kg.addEdge(
+                                get_token_text(subj),
+                                get_token_text(obj),
+                                label=verb_token.text
+                            )
+
+                elif subjects and not objects:
+                    for subj in subjects:
+                        pass
+                        #kg.addNode(get_token_text(subj))
+                        #kg.addEdge(get_token_text(obj), get_token_text(obj))
+
+                elif objects and not subjects:
                     for obj in objects:
-                        if obj.text.strip() and subj.text.strip():
-                            subj_text = subj.text
-                            obj_text = obj.text
-                            if subj.pos_ == 'PRON':
-                                subj_text = replace_pronoun(subj)
-                            if obj.pos_ == 'PRON':
-                                obj_text = replace_pronoun(obj)
-                            kg.addNode(subj_text)
-                            kg.addNode(obj_text)
-                            kg.addEdge(subj_text, obj_text, label=verb_token.text)
+                        pass
+                        #kg.addNode(get_token_text(obj))
+                        #kg.addEdge(get_token_text(obj), get_token_text(obj))
 
-
-if __name__ == '__main__':
-    kg = KnowledgeGraph()
-    text = "The quick brown fox jumped over the lazy, fat dog."
-    translate_text_into_graph(kg, text)
-    kg.show()
 
