bd32903819dfb5477c54ccd1003b0bfeaa4b8a7b
Naz <noreply@github.com>
2022-08-04 09:54:11 -0400

    Use Spacy's tree of dependencies to create graph.

--- src/text_to_graph.py
+++ src/text_to_graph.py
@@ -109,6 +109,13 @@ def text_to_graph_subj_verb_obj(kg, text):
                     print(objects)
 
 
+def add_and_link_nodes(kg, tok1, tok2):
+    kg.addNode(tok1.lemma_)
+    kg.addNode(tok2.lemma_)
+    kg.addEdge(tok1.lemma_, tok2.lemma_)
+
+
+
 def text_to_graph_parse_tree(kg, text):
     """
     Adds nodes and relationships from the sentences into knowledge graph (kg)
@@ -118,18 +125,27 @@ def text_to_graph_parse_tree(kg, text):
     sentences = new_text.split(".")
 
     for sentence in sentences:
-        all_tokens = nlp(sentence)
+        tokens = nlp(sentence)
         accepted_pos = ['NOUN', 'VERB', 'ADJ']
-        tokens = [token for token in all_tokens if token.pos_ in accepted_pos]
+        bypassed_lemmas = ['be']  # link two nodes directly: no "be" inbetween
+        queue = [tok for tok in tokens if tok.dep_ == 'ROOT']
 
-        token_dict = {}
-        for token in tokens:
-            token_dict[token.text] = token
+        def process_child_token(parent_token, token):
+            child_tokens = [token]
+            if token.lemma_ in bypassed_lemmas:
+                child_tokens = [t for t in token.lefts]
+                child_tokens += [t for t in token.rights]
+            for c_tok in child_tokens:
+                queue.append(c_tok)
+                if (token.pos_ in accepted_pos and token.text and
+                    parent_token.lemma_ != token.lemma_ and
+                    token.lemma_ not in bypassed_lemmas):
+                    add_and_link_nodes(kg, parent_token, token)
 
-        for text, token in token_dict.items():
-            if token.head.text and token.text and token.head.lemma_ != token.lemma_:
-                kg.addNode(token.lemma_)
-                kg.addNode(token.head.lemma_)
-                kg.addEdge(token.lemma_, token.head.lemma_)
-
+        while queue:
+            parent_token = queue.pop(0)
+            for token in parent_token.lefts:
+                process_child_token(parent_token, token)
+            for token in parent_token.rights:
+                process_child_token(parent_token, token)
 
