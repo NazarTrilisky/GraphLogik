9eb3597a5fde40776155df7c0bf2fb0d7629cca8
Naz <noreply@github.com>
2022-09-06 22:33:52 -0600

    Fix data loss.  Use token heads to build graph.

--- src/text_to_graph.py
+++ src/text_to_graph.py
@@ -1,15 +1,12 @@
-
 import spacy
 from depronounize.depronounize import replace_pronouns
 from src.graph import KnowledgeGraph
 
 
 nlp = spacy.load('en_core_web_sm')
-stopwords = ['a', 'the', 'for', 'at', 'by']
 
 
 def add_and_link_nodes(kg, tok1, tok2, edge_attrs):
-    #accepted_pos = ['VERB', 'NOUN', 'ADJ', 'NUM', 'DET']
     if (
         tok1.lemma_.lower() != tok2.lemma_.lower() and
         not tok1.is_punct and
@@ -17,61 +14,71 @@ def add_and_link_nodes(kg, tok1, tok2, edge_attrs):
         tok1.text.strip() and
         tok2.text.strip()
     ):
-        kg.addNode(tok1.lemma_.lower())
-        kg.addNode(tok2.lemma_.lower())
-        kg.addEdge(tok1.lemma_.lower(),
-                   tok2.lemma_.lower(),
-                   **edge_attrs)
+        t1 = tok1.text
+        t2 = tok2.text
+        kg.addNode(t1)
+        kg.addNode(t2)
+        kg.addEdge(t1, t2, **edge_attrs)
 
 
-def process_verb(kg, verb_token):
-    subj = []
-    obj = []
-    for t2 in verb_token.subtree:
-        if t2.pos_ in ['VERB', 'AUX'] and verb_token.lemma_ != t2.lemma_:
-            process_verb(kg, t2)
-        elif 'subj' in t2.dep_:
-            for t3 in t2.subtree:
-                subj.append(t3)
-        elif 'obj' in t2.dep_:
-            for t3 in t2.subtree:
-                obj.append(t3)
-
-    #print("\n   %s" % verb_token)
-    #print("subj %s" % subj)
-    #print("obj %s" % obj)
-
-    if subj and obj:
-        for s1 in subj:
-            for o1 in obj:
-                add_and_link_nodes(kg, s1, o1,
-                                   {'name': verb_token.lemma_})
-    elif subj:
-        for s1 in subj:
-            add_and_link_nodes(kg, s1, verb_token,
-                                   {'name': 'action'})
-    elif obj:
-        for o1 in obj:
-            add_and_link_nodes(kg, verb_token, o1,
-                                   {'name': 'action'})
+class Node:
+    def __init__(self, token):
+        self.token = token
+        self.kids = []
 
 
-def build_tree_from_verb(kg, tok):
-    visited = set()
-    queue = [tok]
-    while queue:
-        cur = queue.pop(0)
-        visited.add(cur.lemma_.lower())
-        for t1 in cur.subtree:
-            if t1.lemma_.lower() not in visited:
-                if t1.pos_ in ['VERB', 'AUX']:
-                    build_tree_from_verb(kg, t1)
-                else:
-                    queue.append(t1)
+def build_tree_from_heads(kg, root_tok):
+    """
+    Build a dependency tree for one sentence
+    using each token's head
+    """
+    root_node = Node(root_tok)
+    node_dict = {root_tok.vector_norm: root_node}
 
-        if cur.has_head():
-            add_and_link_nodes(kg, cur, cur.head, {'name': ''})
+    for kid_tok in root_tok.subtree:
+        if kid_tok.vector_norm == root_tok.vector_norm:
+            continue
 
+        if kid_tok.vector_norm not in node_dict:
+            node_dict[kid_tok.vector_norm] = Node(kid_tok)
+
+        if kid_tok.has_head():
+            if kid_tok.head.vector_norm not in node_dict:
+                node_dict[kid_tok.head.vector_norm] = Node(kid_tok.head)
+
+            node_dict[kid_tok.head.vector_norm].kids.append(
+                node_dict[kid_tok.vector_norm])
+
+    return root_node
+
+
+
+SKIPPED_POS = ['ADP', 'CCONJ']
+
+def link_node_to_kids(kg, cur_node, child_nodes):
+    """
+    Don't link two verbs in a row
+    Don't link parts of speech (POS) that do not matter
+    """
+    for child_node in child_nodes:
+        if (not (child_node.token.pos_ in ['VERB', 'AUX'] and
+                 cur_node.token.pos_ in ['VERB', 'AUX']) and
+            child_node.token.pos_ not in SKIPPED_POS and
+            cur_node.token.pos_ not in SKIPPED_POS
+           ):
+            add_and_link_nodes(
+                kg,
+                cur_node.token,
+                child_node.token,
+                {'name': ''}
+            )
+
+        elif child_node.token.pos_ in SKIPPED_POS:
+            if child_node.kids:
+                link_node_to_kids(kg, cur_node, child_node.kids)
+
+        if child_node.kids:
+            link_node_to_kids(kg, child_node, child_node.kids)
 
 
 def text_to_graph_link_all(kg, text):
@@ -86,5 +93,6 @@ def text_to_graph_link_all(kg, text):
         tokens = nlp(sentence)
         for t1 in tokens:
             if t1.dep_ == 'ROOT':
-                process_verb(kg, t1)
+                root_node = build_tree_from_heads(kg, t1)
+                link_node_to_kids(kg, root_node, root_node.kids)
 
