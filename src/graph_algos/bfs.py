aee8371547266b74ac29471a17ba91e45d99144a
Developer <dev@graphlogik.com>
2023-02-05 21:37:30 -0500

    Test BFS graph search

--- src/graph_algos/bfs.py
+++ src/graph_algos/bfs.py
@@ -1,46 +1,45 @@
 
-from collections import defaultdict
 import spacy
 
 from src.visualizer import DisplayGraph
 
 
-def bfs(kg, start_words, max_depth):
+def breadth_first_search(kg, start_words, max_depth):
     """
     arg: start_words = list of strings to start searching graph with
     arg: max_depth = max breadth-first-search depth
     return: dict of visited nodes
         key = node name, val = strength of path to that node
     """
-    counter = 0
+    IGNORED_POS = ['ADP', 'DET', 'CCONJ']
+    POS_TO_DISPLAY = ['NOUN']
+    STEP_WEIGHT_SUBTRACTION = 1
+
+    depth = 0
     dgr = DisplayGraph()
-    TO_DISPLAY_POS = ['NOUN']
+    # tuple (node-name, sum-of-edge-weights-to-node-from-start-words)
+    queue = [(word, 1) for word in start_words]
+    # key = word, val = sum of edge weights to node
+    visited_dict = {k:0 for k in start_words}
 
-    next_dict = {k:1 for k in start_words}
-    visited_dict = {k:1 for k in start_words}
-    IGNORED_POS = ['ADP', 'DET']
+    while queue and depth < max_depth:
+        depth += 1
+        cur_word, cur_weight = queue.pop(0)
+        if cur_word in kg.nodes:
+            dgr.addNode(cur_word)
+            for neighbor_word in kg.nodes[cur_word].edges.keys():
+                if neighbor_word not in visited_dict:
+                    queue.append((neighbor_word,
+                                  kg.nodes[cur_word].edges[neighbor_word]
+                                  + cur_weight
+                                  - STEP_WEIGHT_SUBTRACTION))
+                    if kg.nodes[neighbor_word].attrs['pos_'] not in IGNORED_POS:
+                        visited_dict[neighbor_word] = kg.nodes[cur_word].edges[neighbor_word]
+                if kg.nodes[neighbor_word].attrs['pos_'] in POS_TO_DISPLAY:
+                    dgr.addNode(neighbor_word)
+                    dgr.addEdge(cur_word, neighbor_word)
 
-    while next_dict and counter < max_depth:
-        counter += 1
-        new_next_dict = {}
-        for word in next_dict:
-            if (word not in visited_dict and
-                kg.nodes[word].attrs['pos_'] not in IGNORED_POS):
-                visited_dict[word] = next_dict[word]
-                nxt_dict = get_next_nodes(kg, word)
-                for next_word in nxt_dict:
-                    new_next_dict[next_word] = nxt_dict[next_word]
-                    dgr.addNode(word)
-                    dgr.addNode(next_word)
-                    dgr.addEdge(word, next_word)
-
-        next_dict = new_next_dict
-
-    for key in next_dict:
-        visited_dict[key] = next_dict[key]
-
-
-    #dgr.show()
+    dgr.show()
 
     return visited_dict
 
---
+++ tests/graph_algos/test_bfs.py
@@ -0,0 +1,60 @@
+
+import os
+import sys
+import pathlib
+import warnings
+warnings.filterwarnings("ignore")
+
+sys.path.insert(0, '.')
+from src.graph import KnowledgeGraph
+from src.text_to_graph import text_to_graph_link_all
+from src.graph_algos import bfs
+from src.apis.pdf_to_text import get_text_from_pdf
+
+
+def test_bfs_berkshire_vs_enron():
+    test_file_path = pathlib.Path(__file__).parents[1].resolve()
+    test_file_path = os.path.join(test_file_path, 'files')
+    e_pkl_path = os.path.join(test_file_path, 'enron_2000_graph.pkl')
+    b_pkl_path = os.path.join(test_file_path, 'berkshire_2000_graph.pkl')
+
+    if os.path.exists(e_pkl_path):
+        e_kg = KnowledgeGraph.load_graph(e_pkl_path)
+    else:
+        enron_path = os.path.join(test_file_path, 'EnronAnnualReport2000.pdf')
+        e_txt = get_text_from_pdf(enron_path)
+        e_kg = KnowledgeGraph()
+        text_to_graph_link_all(e_kg, e_txt)
+        e_kg.save_graph(e_pkl_path)
+
+    if os.path.exists(b_pkl_path):
+        b_kg = KnowledgeGraph.load_graph(b_pkl_path)
+    else:
+        berkshire_path = os.path.join(test_file_path, 'BerkshireHathawayAnnualReport2000.pdf')
+        b_txt = get_text_from_pdf(berkshire_path)
+        b_kg = KnowledgeGraph()
+        text_to_graph_link_all(b_kg, b_txt)
+        b_kg.save_graph(b_pkl_path)
+
+    #@todo check combinations
+    #SEARCH_LIST = ['compliance', 'earnings', 'sustainable']
+    SEARCH_LIST = ["debt", "borrow"]
+    SEARCH_LIST = ['loss', 'losses']
+
+    def print_visited(kg_obj):
+        visited_dict = bfs.breadth_first_search(kg_obj, SEARCH_LIST, max_depth=3)
+        top_nodes = [(k, v) for k, v in sorted(visited_dict.items(),
+                     key=lambda item: item[1], reverse=True)]
+        for x in top_nodes[:15]:
+            print(x)
+
+    print("---------------------\nEnron:")
+    print_visited(e_kg)
+    print("---------------------\nBerkshire:")
+    print_visited(b_kg)
+
+
+if __name__ == '__main__':
+    test_bfs_berkshire_vs_enron()
+    print('passed')
+
